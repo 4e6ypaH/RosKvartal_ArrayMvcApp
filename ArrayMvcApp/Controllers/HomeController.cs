ce606ae07c0dd230ae531b341a30bf9400e14259
Maxim Troshkov <maxitm_troshkov@mail.ru>
2018-12-06 19:55:33 +0300

    lesson 31

--- ArrayMvcApp/Controllers/HomeController.cs
+++ ArrayMvcApp/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ArrayMvcApp.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,6 +12,50 @@ namespace ArrayMvcApp.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult GetAuthor()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult GetAuthor(Author author)
+        {
+            return View();
+        }
+
+        //public ActionResult Edit()
+        //{
+        //    List<Book> books = new List<Book>();
+        //    books.Add(new Book { Name = "Война и мир", Author = "Л. Толстой", Price = 220 });
+        //    books.Add(new Book { Name = "Отцы и дети", Author = "И. Тургенев", Price = 180 });
+        //    books.Add(new Book { Name = "Чайка", Author = "А. Чехов", Price = 150 });
+
+        //    return View(books);
+        //}
+
+        //[HttpPost]
+        //public string Edit(List<Book> books)
+        //{
+        //    return books.Count.ToString();
+        //}
+
+        public ActionResult Array()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public string Array(List<string> names)
+        {
+            string fin = "";
+            for (int i = 0; i < names.Count; i++)
+            {
+                fin += names[i] + "; ";
+            }
+            return fin;
         }
 
         public ActionResult About()
